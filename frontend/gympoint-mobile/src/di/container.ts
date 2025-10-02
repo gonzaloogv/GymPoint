6dc7cda2c0ba29c06135d21fe4ff79172d82d1f5
Gonzalo Orlando Gomez Vignudo <gonzaloogv23@gmail.com>
2025-10-02 17:24:26 -0300

    feat(routines): configure dependency injection container

--- frontend/gympoint-mobile/src/di/container.ts
+++ frontend/gympoint-mobile/src/di/container.ts
@@ -13,6 +13,15 @@ import { ListNearbyGyms } from '@features/gyms/domain/usecases/ListNearbyGyms';
 import { ScheduleRepository } from '@features/gyms/domain/repositories/ScheduleRepository';
 import { ScheduleRepositoryImpl } from '@features/gyms/data/ScheduleRepositoryImpl';
 import { GetSchedulesForGyms } from '@features/gyms/domain/usecases/GetSchedulesForGyms';
+
+// ===== Routines =====
+import { RoutineRepository } from '@features/routines/domain/repositories/RoutineRepository';
+import { RoutineRepositoryImpl } from '@features/routines/data/RoutineRepositoryImpl';
+import { RoutineLocal } from '@features/routines/data/datasources/RoutineLocal';
+import { GetRoutines } from '@features/routines/domain/usecases/GetRoutines';
+import { GetRoutineById } from '@features/routines/domain/usecases/GetRoutineById';
+import { ExecuteRoutine } from '@features/routines/domain/usecases/ExecuteRoutine';
+import { GetRoutineHistory } from '@features/routines/domain/usecases/GetRoutineHistory';
 class Container {
   // Auth
   authRepository: AuthRepository;
@@ -27,6 +36,14 @@ class Container {
   scheduleRepository: ScheduleRepository;
   getSchedulesForGyms: GetSchedulesForGyms;
 
+  // Routines
+  routineLocal: RoutineLocal;
+  routineRepository: RoutineRepository;
+  getRoutines: GetRoutines;
+  getRoutineById: GetRoutineById;
+  executeRoutine: ExecuteRoutine;
+  getRoutineHistory: GetRoutineHistory;
+
   constructor() {
     // Auth
     this.authRepository = new AuthRepositoryImpl();
@@ -40,6 +57,14 @@ class Container {
     // Schedules (IMPORTANTE)
     this.scheduleRepository = new ScheduleRepositoryImpl();
     this.getSchedulesForGyms = new GetSchedulesForGyms(this.scheduleRepository);
+
+    // Routines
+    this.routineLocal = new RoutineLocal();
+    this.routineRepository = new RoutineRepositoryImpl(this.routineLocal);
+    this.getRoutines = new GetRoutines(this.routineRepository);
+    this.getRoutineById = new GetRoutineById(this.routineRepository);
+    this.executeRoutine = new ExecuteRoutine(this.routineRepository);
+    this.getRoutineHistory = new GetRoutineHistory(this.routineRepository);
   }
 }
 
